9d7f2d6e906c4ff2c458d57222147361f23ce422
Noah Thompson <noahpthomp@gmail.com>
2021-01-18 14:32:40 -0800

    previous tests pass

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -18,14 +18,17 @@
 
          public static void Information(this BaseLogger logger, string formatMessage, params object[] insertIntoMessage)
         {
+             logger.Log(LogLevel.Information, string.Format(formatMessage, insertIntoMessage));
         }
 
          public static void Warning(this BaseLogger logger, string formatMessage, params object[] insertIntoMessage)
         {
+             logger.Log(LogLevel.Warning, string.Format(formatMessage, insertIntoMessage));
         }
 
          public static void Debug(this BaseLogger logger, string formatMessage, params object[] insertIntoMessage)
         {
+             logger.Log(LogLevel.Debug, string.Format(formatMessage, insertIntoMessage));
         }
     }
 }
