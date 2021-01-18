23f46f35bb81b106bcfa31d0767014a960ee375d
Noah Thompson <noahpthomp@gmail.com>
2021-01-18 12:16:44 -0800

    added BaseLoggerMixins.Error, one overload automatically formats strings. Tests now run and fail

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -2,6 +2,15 @@
 {
     public static class BaseLoggerMixins
     {
-        
+        public static void Error(this BaseLogger logger, string message)
+        {
+
+        }
+
+        //Allows one to write: MyLogger.Error("Some message {0} {1}", 12345, 67890 );
+        public static void Error(this BaseLogger logger, string formatMessage, params object[] insertIntoMessage)
+        {
+            logger.Error(string.Format(formatMessage, insertIntoMessage));
+        }
     }
 }
