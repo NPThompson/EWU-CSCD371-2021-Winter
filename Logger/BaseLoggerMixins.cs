3a9dc8ab40cb20ce50a8ad9b4eae87b95f567e76
Noah Thompson <noahpthomp@gmail.com>
2021-01-18 14:31:22 -0800

    added three tests for the other extension methods (Warning, Information, Debug) - all fail

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -13,7 +13,19 @@
         //Allows one to write: MyLogger.Error("Some message {0} {1}", 12345, 67890 );
         public static void Error(this BaseLogger logger, string formatMessage, params object[] insertIntoMessage)
         {
-            logger.Error(string.Format(formatMessage, insertIntoMessage));
+            logger.Log(LogLevel.Error, string.Format(formatMessage, insertIntoMessage));
+        }
+
+         public static void Information(this BaseLogger logger, string formatMessage, params object[] insertIntoMessage)
+        {
+        }
+
+         public static void Warning(this BaseLogger logger, string formatMessage, params object[] insertIntoMessage)
+        {
+        }
+
+         public static void Debug(this BaseLogger logger, string formatMessage, params object[] insertIntoMessage)
+        {
         }
     }
 }
