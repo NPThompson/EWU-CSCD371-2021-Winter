4c29851f4f173e2254a1cc5892c8f9d7bf8c6dd8
Noah Thompson <noahpthomp@gmail.com>
2021-01-25 14:06:38 -0800

    added jester class

---
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CanHazFunny
+{
+	public class Jester : IJokeService, IJokeOutput
+	{
+		public Jester()
+		{
+		}
+
+		public string GetJoke()
+        {
+			return "";
+        }
+
+		public void outputJoke(string joke)
+        {
+
+        }
+	}
+}
