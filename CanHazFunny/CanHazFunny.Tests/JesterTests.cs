c60fabd08151b9dd871993bf57a4e256755aa099
Noah Thompson <noahpthomp@gmail.com>
2021-01-25 21:51:15 -0800

    Jester_ThrowsException_OnNullService now Jester_ThrowsException_OnNullConstructorParameters, to reflect new constructor. Jester_FilterChuckNorrisJokes reflects case-insensitive test for presence of "ChUcK nOrRiS" or any such variation"

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -8,9 +8,9 @@ namespace CanHazFunny.Tests
     {
      [TestMethod]
      [ExpectedException(typeof(System.ArgumentNullException))]
-     public void Jester_ThrowsException_OnNullJokeService()
+     public void Jester_ThrowsException_OnNullConstructorParameters()
         {
-            Jester jester = new(null!);
+            Jester jester = new(null!, null!);
         }
 
     [TestMethod]
@@ -18,9 +18,9 @@ namespace CanHazFunny.Tests
         {
             Mock<IJokeService> testService = new();
             testService.SetupSequence(testService => testService.GetJoke())
-                .Returns("chuck norris once threw a grenade. It killed fifty people, then it exploded.")
+                .Returns("Chuck norris once threw a grenade. It killed fifty people, then it exploded.")
                 .Returns("why do programmers celebrate halloween and christmas on the same day? Because oct 31 = dec 25!");
-            Jester jester = new(testService.Object);
+            Jester jester = new(new JokeConsoleOutput(), testService.Object);
             string joke = jester.GetJoke();
             Assert.IsFalse(joke.Contains("chuck norris"));
         }
