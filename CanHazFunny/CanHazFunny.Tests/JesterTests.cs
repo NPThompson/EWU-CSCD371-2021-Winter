1d6111b9cff55afa24d823b4b7f0abdd054f9a1d
Noah Thompson <noahpthomp@gmail.com>
2021-01-25 21:28:04 -0800

    Jester_FilterChuckNorrisJokes uses a mock that returns a chuck norris and non chuck norris joke in sequence, when GetJoke is called

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -17,9 +17,12 @@ namespace CanHazFunny.Tests
      public void Jester_FiltersChuckNorrisJokes()
         {
             Mock<IJokeService> testService = new();
+            testService.SetupSequence(testService => testService.GetJoke())
+                .Returns("chuck norris once threw a grenade. It killed fifty people, then it exploded.")
+                .Returns("why do programmers celebrate halloween and christmas on the same day? Because oct 31 = dec 25!");
             Jester jester = new(testService.Object);
-
-            Assert.Fail("Test service should send chuck norris jokes first, then regular ones");
+            string joke = jester.GetJoke();
+            Assert.IsFalse(joke.Contains("chuck norris"));
         }
     }
 }
