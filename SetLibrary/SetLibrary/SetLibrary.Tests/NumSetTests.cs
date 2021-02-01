caa65d8d12e446c7ca33f31a5b5e12ee5f9091ae
Noah Thompson <noahpthomp@gmail.com>
2021-02-01 09:08:12 -0800

    SetLibrary.Tests

---
+++ SetLibrary/SetLibrary/SetLibrary.Tests/NumSetTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SetLibrary.Tests
+{
+    [TestClass]
+    public class NumSetTests
+    {
+        [TestMethod]
+        public void foo()
+        {
+        }
+    }
+}
---
+++ SetLibrary/SetLibrary/SetLibrary.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SetLibrary.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SetLibrary.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f95f23a-fcf3-48dc-b30b-7febf72b6ab9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
