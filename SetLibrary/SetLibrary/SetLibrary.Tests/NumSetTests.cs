0177c3e43891b0d6787a9a1d6c1cec364a3b5853
Noah Thompson <noahpthomp@gmail.com>
2021-02-01 13:36:07 -0800

    NumSet.Equals(), ==, !=, and Appropriate tests

--- SetLibrary/SetLibrary/SetLibrary.Tests/NumSetTests.cs
+++ SetLibrary/SetLibrary/SetLibrary.Tests/NumSetTests.cs
@@ -5,18 +5,51 @@ namespace SetLibrary.Tests
     [TestClass]
     public class NumSetTests
     {
-        [TestMethod]
-        public void NumSet_Constructor_AcceptsParamList()
-        { 
-           NumSet numSet = new(0, 1, 2, 3, 4, 5);
+        NumSet numSet = new();
+     
+        void setup()
+        {
+            numSet = new(0, 1, 2, 3, 4, 5);
         }
 
         [TestMethod]
-        public void NumSet_ContainsMethod_ShowsContainedElement()
+        public void NumSet_Constructor_AcceptsParamList()
         {
-            NumSet numSet = new(0);
+            setup();
+        }
+
+        [TestMethod]
+        public void NumSet_Contains_ShowsContainedElement()
+        {
+            setup();
 
             Assert.IsTrue(numSet.Contains(0));
+        }
+
+        [TestMethod]
+        public void NumSet_Contains_DoesNotShowMissingElement()
+        {
+            setup();
+
+            Assert.IsFalse(numSet.Contains(6));
+        }
+
+        [TestMethod]
+        public void NumSet_AreUnequalToNullOrOtherTypes()
+        {
+            Assert.IsFalse(new NumSet() == null);
+            Assert.IsFalse(null == new NumSet());
+            Assert.IsFalse(new NumSet().Equals(42));
+        }
+
+        [TestMethod]
+        public void NumSet_EqualSets_AreEqualUnderDoubleEqualsOperator()
+        {
+            setup();
+
+            NumSet numSet2 = new(5, 4, 3, 2, 1, 0);
+            
+            Assert.IsTrue(numSet == numSet2);
         }
     }
 }
--- SetLibrary/SetLibrary/SetLibrary/NumSet.cs
+++ SetLibrary/SetLibrary/SetLibrary/NumSet.cs
@@ -23,5 +23,42 @@ namespace SetLibrary
         {
             return Elements.Contains(element);
         }
+
+        
+
+        public static bool operator !=(NumSet numSet1, NumSet numSet2)
+        {
+            return !(numSet1 == numSet2);
+        }
+
+        public static bool operator ==(NumSet? numSet1, NumSet? numSet2)
+        {
+            if (numSet1 is null)
+                return numSet2 is null;
+
+            return numSet1.Equals(numSet2);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null)
+                return false;
+
+            if (obj is not NumSet)
+                return false;
+
+            NumSet other = (NumSet)obj;
+
+            foreach (int i in Elements)
+                if (!other.Contains(i))
+                    return false;
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
